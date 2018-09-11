6fd2a7f501f4216ae45f6ae1d74ac7118287607c
Alex Lapinski <contact@alexlapinski.name>
2018-09-10 21:51:14 -0400

    Initial project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import praw
+
+from settings import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT
+
+reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
+                     client_secret=REDDIT_CLIENT_SECRET,
+                     user_agent=REDDIT_USER_AGENT)
+
+print(reddit.read_only)  # Output: True)
